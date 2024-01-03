97f9c0db9f3466c81c009c25d6212b7397ce6165
Junior <duartejusanca@gmail.com>
2024-01-03 05:48:51 +0100

    todo done, crd not update yet

--- app.js
+++ app.js
@@ -2,39 +2,51 @@ const title = document.createElement('h1')
 title.textContent = 'My shopping list'
 document.body.appendChild(title)
 
+const div = document.createElement('div')
+document.body.appendChild(div)
+
+const ul = document.createElement('ul')
+document.body.appendChild(ul)
+
 const label = document.createElement('label')
 label.textContent = 'Enter a new item: '
 document.body.appendChild(label)
+div.append(label)
 
 const input = document.createElement('input')
 document.body.appendChild(input)
 input.setAttribute('type', 'text')
 input.setAttribute('id', 'item')
 input.setAttribute('name', 'item')
+div.append(input)
 
 const addBtn = document.createElement('button')
 addBtn.textContent = 'Add item'
 document.body.appendChild(addBtn)
+div.append(addBtn)
 
+//---------[ add btn function ]--------//
 addBtn.addEventListener('click', function (){
   const value = input.value;
-  span.textContent = value;
-
-
-  delBtn.textContent = 'Delete';
   document.getElementById('item').value = ''
 
   console.log(value)
+
+  const li = document.createElement('li')
+  const span = document.createElement('span')
+  const delBtn = document.createElement('button')
+
+  li.appendChild(span);
+  span.textContent = value;
+  li.appendChild(delBtn);
+  delBtn.textContent ='Delete';
+  ul.appendChild(li)
+
+  delBtn.addEventListener('click', function(){
+    ul.removeChild(li)
+  });
+
+  input.focus();
+
 })
 
-const ul = document.createElement('ul')
-document.body.appendChild(ul)
-const li = document.createElement('li')
-ul.appendChild(li)
-const span = document.createElement('span')
-li.appendChild(span)
-const delBtn = document.createElement('button')
-li.appendChild(delBtn)
-delBtn.addEventListener('click', function(){
-  li.remove()
-})
